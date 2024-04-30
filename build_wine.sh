7ede2131fefa6df142386ee4f8fde0b62f56207d
tiger-mountain <167862084+tiger-mountain@users.noreply.github.com>
2024-05-01 05:11:53 +0800

    更新 build_wine.sh

--- build_wine.sh
+++ build_wine.sh
@@ -6,16 +6,15 @@ mv "wine-9.7" wine97
 rm -f wine-9.7.tar.xz
 
 mkdir /opt/wine97/build
-cd /opt/wine97/build
-/opt/wine97/configure --enable-archs=i386,x86_64 --prefix /opt/wine97/build/wine-9.7-exp-amd64
-make install
 
-cp /opt/wine97/build/wine-9.7-exp-amd64/bin/wine-preloader /opt/wine97/build/wine-9.7-exp-amd64/bin/wine64-preloader
+/opt/wine97/configure --enable-archs=i386,x86_64 --prefix=/opt/wine-9.7-exp-amd64
+make -C /opt/wine97/build install
 
-cp /opt/wine97/build/wine-9.7-exp-amd64/bin/wine /opt/wine97/build/wine-9.7-exp-amd64/bin/wine64
+cp /opt/wine-9.7-exp-amd64/bin/wine-preloader /opt/wine-9.7-exp-amd64/bin/wine64-preloader
 
-tar -Jcf wine-9.7-exp-amd64.tar.xz wine-9.7-exp-amd64
-mv wine-9.7-exp-amd64.tar.xz /opt/
+cp /opt/wine-9.7-exp-amd64/bin/wine /opt/wine-9.7-exp-amd64/bin/wine64
+
+tar -Jcf /opt/wine-9.7-exp-amd64.tar.xz /opt/wine-9.7-exp-amd64
 
 rm -rf /opt/wine97
 
