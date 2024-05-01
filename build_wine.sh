e5c0e80be6446b9b0d3745bfcaa4b380b6c7fb97
tiger-mountain <167862084+tiger-mountain@users.noreply.github.com>
2024-05-01 08:27:54 +0800

    更新 build_wine.sh

--- build_wine.sh
+++ build_wine.sh
@@ -1,3 +1,120 @@
+
+# Make 64-bit Wine builds with the new WoW64 mode (32-on-64)
+export EXPERIMENTAL_WOW64="${EXPERIMENTAL_WOW64:-false}"
+
+# Set this to a path to your Wine source code (for example, /home/username/wine-custom-src).
+# This is useful if you already have the Wine source code somewhere on your
+# storage and you want to compile it.
+#
+# You can also set this to a GitHub clone url instead of a local path.
+#
+# If you don't want to compile a custom Wine source code, then just leave this
+# variable empty.
+export CUSTOM_SRC_PATH=""
+
+# Set to true to download and prepare the source code, but do not compile it.
+# If this variable is set to true, root rights are not required.
+export DO_NOT_COMPILE="false"
+
+# Set to true to use ccache to speed up subsequent compilations.
+# First compilation will be a little longer, but subsequent compilations
+# will be significantly faster (especially if you use a fast storage like SSD).
+#
+# Note that ccache requires additional storage space.
+# By default it has a 5 GB limit for its cache size.
+#
+# Make sure that ccache is installed before enabling this.
+export USE_CCACHE="false"
+
+export WINE_BUILD_OPTIONS="--without-ldap --without-oss --disable-winemenubuilder --disable-win16 --disable-tests"
+
+# A temporary directory where the Wine source code will be stored.
+# Do not set this variable to an existing non-empty directory!
+# This directory is removed and recreated on each script run.
+export BUILD_DIR="${HOME}"/build_wine
+
+# Change these paths to where your Ubuntu bootstraps reside
+export BOOTSTRAP_X64=/opt/chroots/bionic64_chroot
+export BOOTSTRAP_X32=/opt/chroots/bionic32_chroot
+
+export scriptdir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+export CC="gcc-9"
+export CXX="g++-9"
+
+export CROSSCC_X32="i686-w64-mingw32-gcc"
+export CROSSCXX_X32="i686-w64-mingw32-g++"
+export CROSSCC_X64="x86_64-w64-mingw32-gcc"
+export CROSSCXX_X64="x86_64-w64-mingw32-g++"
+
+export CFLAGS_X32="-march=i686 -msse2 -mfpmath=sse -O2 -ftree-vectorize"
+export CFLAGS_X64="-march=x86-64 -msse3 -mfpmath=sse -O2 -ftree-vectorize"
+export LDFLAGS="-Wl,-O1,--sort-common,--as-needed"
+
+export CROSSCFLAGS_X32="${CFLAGS_X32}"
+export CROSSCFLAGS_X64="${CFLAGS_X64}"
+export CROSSLDFLAGS="${LDFLAGS}"
+
+if [ "$USE_CCACHE" = "true" ]; then
+        export CC="ccache ${CC}"
+        export CXX="ccache ${CXX}"
+
+        export i386_CC="ccache ${CROSSCC_X32}"
+        export x86_64_CC="ccache ${CROSSCC_X64}"
+
+        export CROSSCC_X32="ccache ${CROSSCC_X32}"
+        export CROSSCXX_X32="ccache ${CROSSCXX_X32}"
+        export CROSSCC_X64="ccache ${CROSSCC_X64}"
+        export CROSSCXX_X64="ccache ${CROSSCXX_X64}"
+
+        if [ -z "${XDG_CACHE_HOME}" ]; then
+                export XDG_CACHE_HOME="${HOME}"/.cache
+        fi
+
+        mkdir -p "${XDG_CACHE_HOME}"/ccache
+        mkdir -p "${HOME}"/.ccache
+fi
+
+build_with_bwrap () {
+        if [ "${1}" = "32" ]; then
+                BOOTSTRAP_PATH="${BOOTSTRAP_X32}"
+        else
+                BOOTSTRAP_PATH="${BOOTSTRAP_X64}"
+        fi
+
+        if [ "${1}" = "32" ] || [ "${1}" = "64" ]; then
+                shift
+        fi
+
+    bwrap --ro-bind "${BOOTSTRAP_PATH}" / --dev /dev --ro-bind /sys /sys \
+                  --proc /proc --tmpfs /tmp --tmpfs /home --tmpfs /run --tmpfs /var \
+                  --tmpfs /mnt --tmpfs /media --bind "${BUILD_DIR}" "${BUILD_DIR}" \
+                  --bind-try "${XDG_CACHE_HOME}"/ccache "${XDG_CACHE_HOME}"/ccache \
+                  --bind-try "${HOME}"/.ccache "${HOME}"/.ccache \
+                  --setenv PATH "/bin:/sbin:/usr/bin:/usr/sbin" \
+                        "$@"
+}
+
+if ! command -v git 1>/dev/null; then
+        echo "Please install git and run the script again"
+        exit 1
+fi
+
+if ! command -v autoconf 1>/dev/null; then
+        echo "Please install autoconf and run the script again"
+        exit 1
+fi
+
+if ! command -v wget 1>/dev/null; then
+        echo "Please install wget and run the script again"
+        exit 1
+fi
+
+if ! command -v xz 1>/dev/null; then
+        echo "Please install xz and run the script again"
+        exit 1
+fi
+
 cd /opt
 wget -q --show-progress "https://dl.winehq.org/wine/source/9.x/wine-9.7.tar.xz"
 
