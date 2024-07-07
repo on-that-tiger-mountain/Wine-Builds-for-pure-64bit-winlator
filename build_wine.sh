0ee81e030d903440fd36a4a9b1517ffd735f2037
on-that-tiger-mountain <167862084+on-that-tiger-mountain@users.noreply.github.com>
2024-07-07 11:48:02 +0800

    更新 build_wine.sh

--- build_wine.sh
+++ build_wine.sh
@@ -258,7 +258,7 @@ else
                 BUILD_NAME="${WINE_VERSION}"
                 git clone https://gitlab.winehq.org/wine/wine.git
                 cd wine
-                git checkout 145b4109205c385cf1bd1ecd8a18e47c9f27a9fd
+                git checkout c91f254a7740dacd5cb6bc3f8817cd1806bddeb4
                 cd ..
                 patch -d wine -Np1 < "${scriptdir}"/wpcap.patch
         fi
