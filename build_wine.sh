3f704a96058b61dd35a8cef84733705f7466552f
on-that-tiger-mountain <167862084+on-that-tiger-mountain@users.noreply.github.com>
2024-07-09 07:21:00 +0800

    更新 build_wine.sh

--- build_wine.sh
+++ build_wine.sh
@@ -258,7 +258,7 @@ else
                 BUILD_NAME="${WINE_VERSION}"
                 git clone https://gitlab.winehq.org/wine/wine.git wine
                 cd wine
-                git checkout 37b926b8fe57e3242b0729598f33f0ad69a2c55f
+                git checkout 9422b844b59282db04af533451f50661de56b9ca
                 cd ..
                 patch -d wine -Np1 < "${scriptdir}"/msi-revert.patch
         fi
