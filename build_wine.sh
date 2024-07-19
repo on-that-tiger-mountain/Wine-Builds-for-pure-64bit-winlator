747147dcaf0449b30991989341a2824d1dabee16
on-that-tiger-mountain <167862084+on-that-tiger-mountain@users.noreply.github.com>
2024-07-20 00:46:18 +0800

    更新 build_wine.sh

--- build_wine.sh
+++ build_wine.sh
@@ -258,7 +258,7 @@ else
                 BUILD_NAME="${WINE_VERSION}"
                 git clone https://gitlab.winehq.org/wine/wine.git wine
                 cd wine
-                git checkout faf645d01632e4cf7ef056a86a675a12df39a80e
+                git checkout 20f1a677b664b3d0d3dc038ffb9ab4f4e4aa6274
                 cd ..
                 #patch -d wine -Np1 < "${scriptdir}"/sfc.patch
         fi
