32a70539d9a9afef19feba0fa2d4f2e628dc6580
on-that-tiger-mountain <167862084+on-that-tiger-mountain@users.noreply.github.com>
2024-09-01 18:39:50 +0800

    更新 build_wine.sh

--- build_wine.sh
+++ build_wine.sh
@@ -259,7 +259,7 @@ else
                 git clone https://gitlab.winehq.org/wine/wine.git wine
                 #git clone -b wine-9.2-custom-fixmapshared https://github.com/on-that-tiger-mountain/wine-9.2-custom.git wine
                 cd wine
-                git checkout 010b497cf6491bd3d4e103db3110b24de17d4a31
+                git checkout 551b535cddeea76f9f277558209e99123d454e59
                 cd ..
                 #patch -d wine -Np1 < "${scriptdir}"/sfc.patch
         fi
