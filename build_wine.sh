1d19978db684f92a29de352405f34615c8a917ac
on-that-tiger-mountain <167862084+on-that-tiger-mountain@users.noreply.github.com>
2024-09-05 02:08:11 +0800

    更新 build_wine.sh

--- build_wine.sh
+++ build_wine.sh
@@ -99,8 +99,10 @@ export CROSSCXX_X32="i686-w64-mingw32-g++"
 export CROSSCC_X64="x86_64-w64-mingw32-gcc"
 export CROSSCXX_X64="x86_64-w64-mingw32-g++"
 
-export CFLAGS_X32="-march=i686 -msse2 -mfpmath=sse"
-export CFLAGS_X64="-march=x86-64 -msse3 -mfpmath=sse"
+export CFLAGS_X32="-march=i686"
+export CFLAGS_X64="-march=x86-64"
+#export CFLAGS_X32="-march=i686 -msse2 -mfpmath=sse"
+#export CFLAGS_X64="-march=x86-64 -msse3 -mfpmath=sse"
 export LDFLAGS="-Wl,-O1,--sort-common,--as-needed"
 
 export CROSSCFLAGS_X32="${CFLAGS_X32}"
